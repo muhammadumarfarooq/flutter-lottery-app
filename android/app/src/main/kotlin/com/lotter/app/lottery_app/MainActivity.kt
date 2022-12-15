32cec3693bdae5e3fb4a5a6ce2d1b0ec2ab22982
umar <umar@craveup.com>
2022-12-15 08:34:29 +0500

    initial commit with lottery lost ui

---
+++ android/app/src/main/kotlin/com/lotter/app/lottery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lotter.app.lottery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int x = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lottery App'),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Lottery winning number is $x'),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.error, color: Colors.red, size: 35),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'Better luck next time your number is $x \ntry again',
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
